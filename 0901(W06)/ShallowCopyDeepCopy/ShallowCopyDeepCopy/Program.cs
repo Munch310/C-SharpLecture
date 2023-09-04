c9f6dd205b6ad05ffeb140009c4fdfaff29ca9a6
Munch310 <ghkdlsi@naver.com>
2023-09-04 14:33:52 +0900

    [ADD] 깊은 복사, 얕은 복사 코드

---
+++ 0901(W06)/ShallowCopyDeepCopy/ShallowCopyDeepCopy/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ShallowCopyDeepCopy
+{
+    internal class Program
+    {
+        class MyClass
+        {
+            public int MyField1;
+            public int MyField2;
+
+            public MyClass DeepCopy()
+            {
+                MyClass newCopy = new MyClass();
+                newCopy.MyField1 = this.MyField1;
+                newCopy.MyField2 = this.MyField2;
+                return newCopy;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass source = new MyClass();
+            source.MyField1 = 10;
+            source.MyField2 = 20;
+
+            MyClass target = source.DeepCopy();
+            target.MyField2 = 30;
+
+            Console.WriteLine($"{source.MyField1} {source.MyField2}");
+            Console.WriteLine($"{target.MyField1} {target.MyField2}");
+        }
+    }
+
+    
+}
